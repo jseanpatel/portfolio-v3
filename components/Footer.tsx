4f2ff2ceccae4fb2b1a3d5552c0c39ccb256f8ee
Jacob Patel <jseanpatel@gmail.com>
2022-05-08 22:36:53 -0700

    chore: Footer weak

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -16,11 +16,10 @@ export default function Footer() {
         </div>
         <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <Link href="/">{siteMetadata.title}</Link>
-          <div>{siteMetadata.author}</div>
           <div>{` • `}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
           <div>{` • `}</div>
-          <Link href="/">{siteMetadata.title}</Link>
+          <Link href="/">{siteMetadata.author}</Link>
         </div>
       </div>
     </footer>
