19b193fd4f9bdf0d8ce5cf693442f4f0f3d350d0
Lucas Patel <lucaspatel@gmail.com>
2022-08-04 22:15:15 -0700

    fix: fixed kbar

--- components/CommandPalette.js
+++ components/CommandPalette.js
@@ -15,9 +15,165 @@ import {
   KBarResults,
   useMatches,
 } from 'kbar';
-import { useEffect } from 'react';
 
-const App = () => {
+// eslint-disable-next-line react/display-name
+const ResultItem = React.forwardRef(
+  ({ action, active, currentRootActionId }, ref) => {
+    const ancestors = React.useMemo(() => {
+      if (!currentRootActionId) return action.ancestors;
+      const index = action.ancestors.findIndex(
+        (ancestor) => ancestor.id === currentRootActionId,
+      );
+      // +1 removes the currentRootAction; e.g.
+      // if we are on the "Set theme" parent action,
+      // the UI should not display "Set theme… > Dark"
+      // but rather just "Dark"
+      return action.ancestors.slice(index + 1);
+    }, [action.ancestors, currentRootActionId]);
+
+    return (
+      <div
+        ref={ref}
+        className={`border-l-2 ${
+          active
+            ? 'bg-gray-200 dark:bg-gray-700 dark:border-white border-gray-700'
+            : 'bg-transparent border-transparent '
+        }`}
+        style={{
+          padding: '12px 16px',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          cursor: 'pointer',
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            gap: '8px',
+            alignItems: 'center',
+            fontSize: 14,
+          }}
+        >
+          {action.icon && action.icon}
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <div>
+              {ancestors.length > 0 &&
+                ancestors.map((ancestor) => (
+                  <React.Fragment key={ancestor.id}>
+                    <span
+                      style={{
+                        opacity: 0.5,
+                        marginRight: 8,
+                      }}
+                    >
+                      {ancestor.name}
+                    </span>
+                    <span
+                      style={{
+                        marginRight: 8,
+                      }}
+                    >
+                      &rsaquo;
+                    </span>
+                  </React.Fragment>
+                ))}
+              <span>{action.name}</span>
+            </div>
+            {action.subtitle && (
+              <span style={{ fontSize: 12 }}>{action.subtitle}</span>
+            )}
+          </div>
+        </div>
+        {action.shortcut?.length ? (
+          <div
+            aria-hidden
+            style={{ display: 'grid', gridAutoFlow: 'column', gap: '4px' }}
+          >
+            {action.shortcut.map((sc) => (
+              <kbd
+                key={sc}
+                style={{
+                  padding: '4px 6px',
+                  background: 'rgba(0 0 0 / .1)',
+                  borderRadius: '4px',
+                  fontSize: 14,
+                }}
+              >
+                {sc}
+              </kbd>
+            ))}
+          </div>
+        ) : null}
+      </div>
+    );
+  },
+);
+
+export const CommandBar = () => {
+  const searchStyle = {
+    padding: '12px 16px',
+    fontSize: '16px',
+    width: '100%',
+    boxSizing: 'border-box',
+    outline: 'none',
+    border: 'none',
+  };
+
+  const animatorStyle = {
+    maxWidth: '600px',
+    width: '100%',
+    borderRadius: '8px',
+    overflow: 'hidden',
+  };
+
+  return (
+    <KBarPortal>
+      <KBarPositioner>
+        <KBarAnimator
+          style={animatorStyle}
+          className="text-gray-700 bg-white shadow-2xl dark:bg-commandPalette dark:text-white"
+        >
+          <KBarSearch
+            style={searchStyle}
+            className="text-gray-700 bg-gray-200 dark:bg-gray-700 dark:text-white"
+          />
+          <RenderResults className="" />
+        </KBarAnimator>
+      </KBarPositioner>
+    </KBarPortal>
+  );
+};
+
+function RenderResults() {
+  const { results, rootActionId } = useMatches();
+
+  const groupNameStyle = {
+    padding: '8px 16px',
+    fontSize: '10px',
+    textTransform: 'uppercase',
+    opacity: 0.5,
+  };
+
+  return (
+    <KBarResults
+      items={results}
+      onRender={({ item, active }) =>
+        typeof item === 'string' ? (
+          <div style={groupNameStyle}>{item}</div>
+        ) : (
+          <ResultItem
+            action={item}
+            active={active}
+            currentRootActionId={rootActionId}
+          />
+        )
+      }
+    />
+  );
+}
+
+export const CommandProvider = (props) => {
   const router = useRouter();
   const { setTheme } = useTheme();
   const { locale } = router;
@@ -164,7 +320,7 @@ const App = () => {
       perform: () => setTheme('dark'),
     },
   ];
-
+  const { children } = props;
   return (
     <KBarProvider
       options={{
@@ -172,166 +328,7 @@ const App = () => {
       }}
       actions={initialActions}
     >
-      <CommandBar />
+      {children}
     </KBarProvider>
   );
 };
-
-function CommandBar() {
-  const searchStyle = {
-    padding: '12px 16px',
-    fontSize: '16px',
-    width: '100%',
-    boxSizing: 'border-box',
-    outline: 'none',
-    border: 'none',
-  };
-
-  const animatorStyle = {
-    maxWidth: '600px',
-    width: '100%',
-    borderRadius: '8px',
-    overflow: 'hidden',
-  };
-
-  return (
-    <KBarPortal>
-      <KBarPositioner>
-        <KBarAnimator
-          style={animatorStyle}
-          className="text-gray-700 bg-white shadow-2xl dark:bg-commandPalette dark:text-white"
-        >
-          <KBarSearch
-            style={searchStyle}
-            className="text-gray-700 bg-gray-200 dark:bg-gray-700 dark:text-white"
-          />
-          <RenderResults className="" />
-        </KBarAnimator>
-      </KBarPositioner>
-    </KBarPortal>
-  );
-}
-
-function RenderResults() {
-  const { results, rootActionId } = useMatches();
-
-  const groupNameStyle = {
-    padding: '8px 16px',
-    fontSize: '10px',
-    textTransform: 'uppercase',
-    opacity: 0.5,
-  };
-
-  return (
-    <KBarResults
-      items={results}
-      onRender={({ item, active }) =>
-        typeof item === 'string' ? (
-          <div style={groupNameStyle}>{item}</div>
-        ) : (
-          <ResultItem
-            action={item}
-            active={active}
-            currentRootActionId={rootActionId}
-          />
-        )
-      }
-    />
-  );
-}
-
-// eslint-disable-next-line react/display-name
-const ResultItem = React.forwardRef(
-  ({ action, active, currentRootActionId }, ref) => {
-    const ancestors = React.useMemo(() => {
-      if (!currentRootActionId) return action.ancestors;
-      const index = action.ancestors.findIndex(
-        (ancestor) => ancestor.id === currentRootActionId,
-      );
-      // +1 removes the currentRootAction; e.g.
-      // if we are on the "Set theme" parent action,
-      // the UI should not display "Set theme… > Dark"
-      // but rather just "Dark"
-      return action.ancestors.slice(index + 1);
-    }, [action.ancestors, currentRootActionId]);
-
-    return (
-      <div
-        ref={ref}
-        className={`border-l-2 ${
-          active
-            ? 'bg-gray-200 dark:bg-gray-700 dark:border-white border-gray-700'
-            : 'bg-transparent border-transparent '
-        }`}
-        style={{
-          padding: '12px 16px',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          cursor: 'pointer',
-        }}
-      >
-        <div
-          style={{
-            display: 'flex',
-            gap: '8px',
-            alignItems: 'center',
-            fontSize: 14,
-          }}
-        >
-          {action.icon && action.icon}
-          <div style={{ display: 'flex', flexDirection: 'column' }}>
-            <div>
-              {ancestors.length > 0 &&
-                ancestors.map((ancestor) => (
-                  <React.Fragment key={ancestor.id}>
-                    <span
-                      style={{
-                        opacity: 0.5,
-                        marginRight: 8,
-                      }}
-                    >
-                      {ancestor.name}
-                    </span>
-                    <span
-                      style={{
-                        marginRight: 8,
-                      }}
-                    >
-                      &rsaquo;
-                    </span>
-                  </React.Fragment>
-                ))}
-              <span>{action.name}</span>
-            </div>
-            {action.subtitle && (
-              <span style={{ fontSize: 12 }}>{action.subtitle}</span>
-            )}
-          </div>
-        </div>
-        {action.shortcut?.length ? (
-          <div
-            aria-hidden
-            style={{ display: 'grid', gridAutoFlow: 'column', gap: '4px' }}
-          >
-            {action.shortcut.map((sc) => (
-              <kbd
-                key={sc}
-                style={{
-                  padding: '4px 6px',
-                  background: 'rgba(0 0 0 / .1)',
-                  borderRadius: '4px',
-                  fontSize: 14,
-                }}
-              >
-                {sc}
-              </kbd>
-            ))}
-          </div>
-        ) : null}
-      </div>
-    );
-  },
-);
-
-export default App;
